ac77510a1556a4c2596de6450d3a4fcee004fefb
Karl Goeltner <kgicetech@gmail.com>
2021-10-30 17:35:10 -0700

    Add sample fetch code from backend api

--- restaurant-tinder-app/src/App.js
+++ restaurant-tinder-app/src/App.js
@@ -2,6 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // Sample fetch from the backend API
+  fetch("http://localhost:3000/restaurants")
+    .then((res) => res.json())
+    .then((json) => {
+        console.log(json);
+        document.getElementById("test").innerHTML = json;
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +21,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div id="test" style={{height:"200px", width:"1200px", wordWrap:"break-word", fontSize:"15px"}}></div>
         <a
           className="App-link"
           href="https://reactjs.org"
