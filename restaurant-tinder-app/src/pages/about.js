c78d10c27351a3ea4083dad13df2ffcae1e92bfa
Kalico <kellyyang33@macbook-pro.lan>
2021-11-19 18:47:20 -0800

    Layout and structure of User Profile page and About page.

--- restaurant-tinder-app/src/pages/about.js
+++ restaurant-tinder-app/src/pages/about.js
@@ -1,17 +1,59 @@
 import React from "react";
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
+import app_logo from '../images/app_logo.png';
+import './about.css'
+
 
 const About = () => {
     return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh'
-            }}
-        >
-            <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, totam.</h1>
+        <div>
+            <br/>
+            <div>
+                <h1>About SCRAMBLE!</h1>
+                <p> A React app for finding restaurants according to your preferences, so you don't have to . . . scramble to find a match.</p>
+            </div>
+
+            <div>
+                <h1>Creators</h1>
+            </div>
+
+
+            <div>
+                <CardGroup>
+                    <Card>
+                        <Card.Img src={app_logo}/>
+                        <br/>
+                        <Card.Title className="name">Karl</Card.Title>
+                    </Card>
+
+                    <Card>
+                        <Card.Img src={app_logo}/>
+                        <br/>
+                        <Card.Title className="name">Tim</Card.Title>
+                    </Card>
+
+                    <Card>
+                        <Card.Img src={app_logo}/>
+                        <br/>
+                        <Card.Title className="name">Eric</Card.Title>
+                    </Card>
+
+                    <Card>
+                        <Card.Img src={app_logo}/>
+                        <br/>
+                        <Card.Title className="name">Rudy</Card.Title>
+                    </Card>
+
+                    <Card>
+                        <Card.Img src={app_logo}/>
+                        <br/>
+                        <Card.Title className="name">Kelly</Card.Title>
+                    </Card>
+                    
+                </CardGroup>
         </div>
+       </div>
     );
 };
 
--- restaurant-tinder-app/src/pages/profile.js
+++ restaurant-tinder-app/src/pages/profile.js
@@ -1,16 +1,33 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import {Link } from "react-router-dom";
+import test from '../images/app_logo.png'
+
+var user={
+    email: "test@gmail.com",
+    password: "password",
+    image: '../images/app_logo.png'
+
+};
 
 function Profile() {
     return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh'
-            }}
-        >
-           <h1>Contact Us</h1> 
+        <div style ={{display: 'flex', justifyContent:'center'}}>
+            <Card style={{ width: '25rem'}}>
+                <Card.Body>
+                    <Card.Title style={{align: 'center'}}>User's Scramble! Profile</Card.Title>
+                    <Card.Img variant="top" src={test}/>
+
+                        <Card.Text>
+                            Email: {user.email}
+                            <br/>
+                            Password: {user.password}
+                        </Card.Text>
+                    <Link to="/filter"><Button variant="primary">Liked and Disliked Restaurants</Button></Link>
+                    <Link to="/filter"><Button variant="primary">User's Created Filters</Button></Link>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
