7757096048497da5efda4b65c2482aba153e46d6
Karl Goeltner <kgicetech@gmail.com>
2021-11-20 17:33:56 -0800

    Connect like/dislike to db

--- restaurant-tinder-app/src/pages/rest_card.js
+++ restaurant-tinder-app/src/pages/rest_card.js
@@ -6,16 +6,17 @@ import axios from "axios";
 import like from '../images/like.png';
 import dislike from '../images/dislike.png';
 import yelp from '../images/yelp.png';
+import gudetama from '../images/gudetama.png';
 
 function Category(props) {
   return (
     <div style={{
           backgroundColor: '#b5f1f5',
           width: 'auto',
-          padding: '5px',
+          padding: '10px',
           marginLeft: '10px',
           marginRight: '5px',
-          borderRadius: '10px'}}>
+          borderRadius: '20px'}}>
       {props.name}
     </div>
   );
@@ -26,11 +27,23 @@ function Transaction(props) {
     <div style={{
           backgroundColor: '#c7f5b5',
           width: 'auto',
-          padding: '5px',
+          padding: '10px',
           marginLeft: '10px',
           marginRight: '5px',
-          borderRadius: '10px'}}>
+          borderRadius: '20px',
+          textTransform: 'capitalize'}}>
       {props.name}
+    </div>
+  );
+}
+
+function BackgroundCard() {
+  return (
+    <div id="bgcard" style={{display: 'none'}}>
+      <a href="http://localhost:3000/filter" style={{position: 'absolute', top: '250px', left: '570px'}}>
+        <img src={gudetama} style={{position: 'absolute', contentAlign: 'center', width: '300px'}}></img>
+      </a>
+      <h3 style={{position: 'absolute', contentAlign: 'center', top: '520px', left: '540px'}}>well, you're really quite picky...</h3>
     </div>
   );
 }
@@ -64,15 +77,45 @@ function RestaurantCard() {
       currentIndexRef.current = val
     }
 
-    // Decrease current index and hide card
-    const swiped = (direction, nameToDelete, index) => {
+    // Decrease current index, hide card, store into DB
+    const swiped = (direction, restaurant, index) => {
+      console.log("currentIndex: " + index);
+      console.log("restaurant: " + restaurant);
+      console.log("direction: " + direction);
+
+      let obj = {
+        username: "karl",
+        alias: restaurant.key,
+        status: direction,
+        name: restaurant.title,
+        address: restaurant.address
+      };
+      
+      // Store restaurant object into database
+      axios
+        .post("http://localhost:3001/update/", obj)
+        .then((res) => console.log(res.data));
+
+      // Display final background card
+      if (index == 1) {
+        document.getElementById('bgcard').style.display = 'block';
+      }
+
+      // Make current card disappear
       document.getElementById("container-" + index).style.visibility = 'hidden';
-      updateCurrentIndex(index - 1)
+      updateCurrentIndex(index - 1);
     }
 
     // Swipe card a certain direction
-    const swipe = async (dir) => {
-      console.log("currentIndex: " + currentIndex);
+    const swipe = async (restaurant, dir) => {
+      // console.log("currentIndex: " + currentIndex);
+      // console.log("restaurant: " + restaurant);
+      // console.log("direction: " + dir);
+
+      if (currentIndex == 1) {
+        document.getElementById('bgcard').style.display = 'block';
+      }
+
       if (currentIndex <= restaurants.length) {
         await childRefs[currentIndex].current.swipe(dir) // Swipe the card!
         console.log('You swiped: ' + dir);
@@ -93,10 +136,12 @@ function RestaurantCard() {
                 height: '80vh'
             }}
         >
-          <div className='cardContainer' style={{position: 'flex', contentAlign: 'center', width: '1000px', height: '520px'}}> 
+          
+          <BackgroundCard id="bgcard"></BackgroundCard>
+          <div className='cardContainer' style={{position: 'flex', contentAlign: 'center', width: '1000px', height: '550px'}}> 
           {restaurants.map((restaurant, index) => (
-            <TinderCard ref={childRefs[index]} flickOnSwipe={true} onSwipe={(dir) => swiped(dir, restaurant.title, index)} onCardLeftScreen={() => onCardLeftScreen('fooBar')} style={{width: '1000px', height: '520px'}}>
-              <div id={"container-" + index} className="wrapper card" style={{height: '520px', width: '1000px', padding: '50px', backgroundColor: 'white', borderRadius: '10px'}}>
+            <TinderCard ref={childRefs[index]} flickOnSwipe={true} onSwipe={(dir) => swiped(dir, restaurant, index)} onCardLeftScreen={() => onCardLeftScreen(index)} style={{width: '1000px', height: '550px'}}>
+              <div id={"container-" + index} className="wrapper card" style={{height: '550px', width: '1000px', padding: '50px', backgroundColor: 'white', borderRadius: '10px'}}>
                 <div className="row">
                   <div className="column">
                     <h2 className="card-title">{restaurant.title}</h2>
@@ -125,7 +170,7 @@ function RestaurantCard() {
                     <h5 style={{marginBottom: '25px'}}>{restaurant.phone}</h5>
                     <div className="row" style={{marginBottom: '20px'}}>
                       {!restaurant.transactions ? '' : restaurant.transactions.map((transaction, index) => (
-                        <Transaction name={transaction}></Transaction>
+                        <Transaction name={transaction.split('_')}></Transaction>
                       ))}
                     </div>
                     <div className="row">
@@ -139,10 +184,10 @@ function RestaurantCard() {
                     </a>
                   </div>
                   <div className="column">
-                    <img style={{borderRadius: '20px', height: '370px', width: '470px', marginTop: '50px', objectFit: 'cover'}} src={restaurant.image} alt="food"></img>
+                    <img style={{borderRadius: '20px', height: '370px', width: '470px', marginTop: '40px', objectFit: 'cover'}} src={restaurant.image} alt="food"></img>
                     <div className="row wrapper">
-                      <img className="likeDislike" src={dislike} alt="dislike" onClick={() => swipe('left')}></img>
-                      <img className="likeDislike" src={like} alt="like" onClick={() => swipe('right')}></img>
+                      <img className="likeDislike" src={dislike} alt="dislike" onClick={() => swipe(restaurant.key, 'left')}></img>
+                      <img className="likeDislike" src={like} alt="like" onClick={() => swipe(restaurant.key, 'right')}></img>
                     </div>
                   </div>
                 </div>
