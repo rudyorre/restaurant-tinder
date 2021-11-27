abd25e26231117fd605366d11884154c83c57ed4
Karl Goeltner <kgicetech@gmail.com>
2021-11-27 14:31:10 -0800

    Add notes card to routes

--- restaurant-tinder-app/src/App.js
+++ restaurant-tinder-app/src/App.js
@@ -10,7 +10,7 @@ import SignUp from './pages/signup.js';
 import SignIn from './pages/signin.js';
 import Filter from './pages/filter.js';
 import RestaurantCard from './pages/rest_card.js';
-import LikedDisliked from './pages/profile_restaurants.js';
+import NotesCard from './pages/notes_card.js';
 
 function App() {
   // Sample fetch from the backend API
@@ -33,7 +33,7 @@ function App() {
         <Route path="/sign-up" element={<SignUp/>} /><Route/>
         <Route path="/filter" element={<Filter />} /><Route/>
         <Route path="/rest_card" element={<RestaurantCard />} /><Route/>
-        <Route path="/profile_restaurants" element={<LikedDisliked />} /><Route/>
+        <Route path="/notes_card" element={<NotesCard />} /><Route/>
       </Routes>
     </Router>
   );
