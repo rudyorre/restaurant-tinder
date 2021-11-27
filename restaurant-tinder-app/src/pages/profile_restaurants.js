e146bbb7775b177c1e01402073fb07eb04910b2a
Eric Zhang <ericzhang11@g.ucla.edu>
2021-11-27 01:43:03 -0800

    add Liked and Disliked buttons

--- restaurant-tinder-app/src/pages/profile_restaurants.js
+++ restaurant-tinder-app/src/pages/profile_restaurants.js
@@ -2,6 +2,7 @@ import React from 'react'
 //import ImageList from '@mui/material/ImageList'
 //import ImageListItem from '@mui/material/ImageListItem'
 //import ImageListItemBar from '@mui/material/ImageListItemBar'
+import './profile_restaurants.css'
 import axios from 'axios'
 
 class LikedDisliked extends React.Component {
@@ -10,13 +11,28 @@ class LikedDisliked extends React.Component {
         this.state = {
             username: '',
             restaurants: {},
+            showLiked: true,
         };
     }
 
     render() {
         return (
-            <div style ={{display: 'flex', justifyContent:'center'}}>
-                <h1>Liked Restaurants</h1>
+            <div style ={{
+                display: 'flex', 
+                justifyContent:'center',
+                height:'100vh'
+            }}>
+                <h1>My Restaurants</h1>
+                <br />
+                <label>
+                    <input type="radio" name="restaurantlist" value="liked" defaultChecked/>
+                    Liked
+                </label>
+                <label>
+                    <input type="radio" name="restaurantlist" value="liked"/>
+                    Disliked
+                </label>
+                
             </div>
         )
     }
