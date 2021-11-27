29e8f57379f07b6bd67413b15285700b51b1f81b
Eric Zhang <ericzhang11@g.ucla.edu>
2021-11-27 10:53:59 -0800

    radio button changes state to display

--- restaurant-tinder-app/src/pages/profile_restaurants.js
+++ restaurant-tinder-app/src/pages/profile_restaurants.js
@@ -4,6 +4,7 @@ import React from 'react'
 //import ImageListItemBar from '@mui/material/ImageListItemBar'
 import './profile_restaurants.css'
 import axios from 'axios'
+import { FaThemeisle } from 'react-icons/fa';
 
 class LikedDisliked extends React.Component {
     constructor(props) {
@@ -15,6 +16,14 @@ class LikedDisliked extends React.Component {
         };
     }
 
+    handleChange = e => {
+        const { value } = e.target;
+    
+        this.setState({
+          showLiked: value=='liked'? true : false,
+        });
+      };
+
     render() {
         return (
             <div style ={{
@@ -25,11 +34,24 @@ class LikedDisliked extends React.Component {
                 <h1>My Restaurants</h1>
                 <br />
                 <label>
-                    <input type="radio" name="restaurantlist" value="liked" defaultChecked/>
+                    <input 
+                        type="radio"
+                        name="restaurantlist"
+                        value="liked"
+                        //checked={this.state.showLiked=true}
+                        onChange={this.handleChange}
+                        defaultChecked
+                    />
                     Liked
                 </label>
                 <label>
-                    <input type="radio" name="restaurantlist" value="liked"/>
+                    <input
+                        type="radio"
+                        name="restaurantlist"
+                        value="disliked"
+                        //checked={this.state.showLiked=false}
+                        onChange={this.handleChange}
+                    />
                     Disliked
                 </label>
                 
