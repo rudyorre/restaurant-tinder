eb0045e491da94cabb2bdd8e0107ce30b39d4213
Eric Zhang <ericzhang11@g.ucla.edu>
2021-11-28 23:07:56 -0800

    made restaurants array part of state

--- restaurant-tinder-app/src/pages/profile_restaurants.js
+++ restaurant-tinder-app/src/pages/profile_restaurants.js
@@ -6,14 +6,13 @@ import './profile_restaurants.css'
 import axios from 'axios'
 import { FaThemeisle } from 'react-icons/fa';
 
-//empty array to contain restaurants to display
-const arr = [];
 
 class LikedDisliked extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showLiked: true,
+      showLiked: null,
+      restaurants: [],
     };
   }
 
@@ -34,7 +33,7 @@ class LikedDisliked extends React.Component {
       "/" + "left").then(response => {return response.data})
     };
 
-    arr.splice(0, arr.length);
+    const arr = [];
     let results = this.state.showLiked ? getLiked() : getDisliked();
 
     results.then(function(result) {
@@ -46,6 +45,7 @@ class LikedDisliked extends React.Component {
           }
           arr.push(restAttributes);
       }
+      return arr;
 
     }).then(arr => {
       this.setState({
@@ -54,7 +54,6 @@ class LikedDisliked extends React.Component {
     }).then(
       console.log(this.state.restaurants)
     );
-
   };
 
   render() {
@@ -73,7 +72,6 @@ class LikedDisliked extends React.Component {
               name="restaurantlist"
               value="liked"
               onChange={this.handleChange}
-              defaultChecked
             />
             Liked
           </label>
@@ -92,12 +90,12 @@ class LikedDisliked extends React.Component {
         
         <div>
           <br />
-          <ImageList sx={{ width: 550, height: '80%' }}>
-            {arr?.map((restaurant) => (
+          <ImageList cols={3} sx={{ width: 750, height: '80%' }}>
+            {this.state.restaurants?.map((restaurant) => (
               <ImageListItem key={restaurant.img}>
                 <img
-                  src={`${restaurant.img}?w=248&fit=crop&auto=format`}
-                  srcSet={`${restaurant.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  src={`${restaurant.img}?w=248&h=100&fit=crop&auto=format`}
+                  srcSet={`${restaurant.img}?w=248&h=100&fit=crop&auto=format&dpr=2 2x`}
                   alt={restaurant.name}
                   loading="lazy"
                 />
@@ -114,79 +112,5 @@ class LikedDisliked extends React.Component {
     )
   }
 }
-
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-    author: '@bkristastucchio',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    author: '@helloimnik',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    author: '@nolanissac',
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    author: '@hjrc33',
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-    author: '@tjdragotta',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-    author: '@katie_wasserman',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-    author: '@silverdalex',
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-    author: '@shelleypauls',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-    author: '@peterlaster',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-    author: '@southside_customs',
-    cols: 2,
-  },
-];
 
 export default LikedDisliked;
