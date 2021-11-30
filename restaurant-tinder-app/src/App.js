17fe4da1ccb5fb79a9ec862a7df87486aaded5be
Karl Goeltner <kgicetech@gmail.com>
2021-11-29 22:47:29 -0800

    Add conditional routing to App.js

--- restaurant-tinder-app/src/App.js
+++ restaurant-tinder-app/src/App.js
@@ -30,23 +30,36 @@ function App() {
     document.title = "Scramble"
   }, [])
 
-  return (
-    <Router>
-      <Navbar isLoggedIn={true}/>
-      <Routes>
-        <Route path="/" exact element={<Login />} /><Route/>
-        <Route path="/about" element={<About/>} /><Route/>
-        <Route path="/profile" element={<Profile/>} /><Route/>
-        <Route path="/signin" element={<SignIn/>} /><Route/>
-        <Route path="/sign-up" element={<SignUp/>} /><Route/>
-        <Route path="/login" element={<Login/>} /><Route/>
-        <Route path="/filter" element={<Filter setFilterValue={setFilterValue} />} /><Route/>
-        <Route path="/rest_card" element={<RestaurantCard filterValue={filter} />} /><Route/>
-        <Route path="/profile_restaurants" element={<LikedDisliked setIndRestaurant={setIndRestaurant} />} /><Route/>
-        <Route path="/notes_card" element={<NotesCard indRestaurantValue={indRestaurant} />} /><Route/>
-      </Routes>
-    </Router>
-  );
+  if (document.cookie != "default") {
+    return (
+      <Router>
+        <Navbar isLoggedIn={true}/>
+        <Routes>
+          <Route path="/" exact element={<Login />} /><Route/>
+            <Route path="/about" element={<About/>} /><Route/>
+            <Route path="/profile" element={<Profile/>} /><Route/>
+            {/* <Route path="/signin" element={<SignIn/>} /><Route/>
+            <Route path="/sign-up" element={<SignUp/>} /><Route/> */}
+            <Route path="/login" element={<Login/>} /><Route/>
+            <Route path="/filter" element={<Filter setFilterValue={setFilterValue} />} /><Route/>
+            <Route path="/rest_card" element={<RestaurantCard filterValue={filter} />} /><Route/>
+            <Route path="/profile_restaurants" element={<LikedDisliked setIndRestaurant={setIndRestaurant} />} /><Route/>
+            <Route path="/notes_card" element={<NotesCard indRestaurantValue={indRestaurant} />} /><Route/>)
+        </Routes>
+      </Router>
+    );
+  } else {
+    return (
+      <Router>
+        <Navbar isLoggedIn={false}/>
+        <Routes>
+          <Route path="/" exact element={<Login />} /><Route/>
+          <Route path="/login" element={<Login/>} /><Route/>
+          <Route path="*" element={<Login/>}></Route>
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
