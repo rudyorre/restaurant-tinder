79d6b1b403a72028bfbb56572e0425fbd4a693bc
ezhang6811 <eric.zhang6811@gmail.com>
2021-12-01 16:13:01 -0800

    create visual for no restaurants to display

--- restaurant-tinder-app/src/pages/profile_restaurants.js
+++ restaurant-tinder-app/src/pages/profile_restaurants.js
@@ -6,6 +6,7 @@ import './profile_restaurants.css'
 import axios from 'axios'
 import { Link } from "react-router-dom";
 import { FaThemeisle } from 'react-icons/fa';
+import eggEmpty from '../images/egg_empty.gif'
 
 
 class LikedDisliked extends React.Component {
@@ -27,40 +28,40 @@ class LikedDisliked extends React.Component {
     const { value } = e.target;
 
     // functions that retrieve liked and disliked restaurants from backend
-    let getLiked = function(){
-      return axios.get("http://localhost:3001/find/status/" + document.cookie + 
-      "/" + "right").then(response => {return response.data})
+    let getLiked = function () {
+      return axios.get("http://localhost:3001/find/status/" + document.cookie +
+        "/" + "right").then(response => { return response.data })
     };
 
-    let getDisliked = function(){
-      return axios.get("http://localhost:3001/find/status/" + document.cookie + 
-      "/" + "left").then(response => {return response.data})
+    let getDisliked = function () {
+      return axios.get("http://localhost:3001/find/status/" + document.cookie +
+        "/" + "left").then(response => { return response.data })
     };
 
     const shuffleArray = (array) => {
       for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          const temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
       }
     }
 
-    let results = value=='liked' ? getLiked() : getDisliked();
+    let results = value == 'liked' ? getLiked() : getDisliked();
     const arr = [];
 
     // push data from each retrieved restaurant into array
-    results.then(function(result) {
+    results.then(function (result) {
       for (let i = 0; i < result.length; i++) {
-          let restAttributes = {
-            alias: result[i].alias,
-            name: result[i].name,
-            img: result[i].image,
-            address: result[i].address,
-            order: result[i].order,
-            status: result[i].status,
-          }
-          arr.push(restAttributes);
+        let restAttributes = {
+          alias: result[i].alias,
+          name: result[i].name,
+          img: result[i].image,
+          address: result[i].address,
+          order: result[i].order,
+          status: result[i].status,
+        }
+        arr.push(restAttributes);
       }
       shuffleArray(arr);
       return arr;
@@ -90,6 +91,44 @@ class LikedDisliked extends React.Component {
     let restaurantList = this.state.restaurants
       .filter(d => this.state.textInput === '' || d.name.toLowerCase().includes(this.state.textInput.toLowerCase()));
 
+    const restaurantExists = restaurantList.length > 0;
+    let pageDisplay;
+
+    if (restaurantExists) {
+      pageDisplay = 
+      <div style = {{ marginLeft: '4.5%' }}>
+        <br />
+      <ImageList cols={3} sx={{ width: '1300px' }}>
+        {restaurantList?.map((restaurant) => (
+          <ImageListItem key={restaurant.img} style={{ marginBottom: '40px' }}>
+            <Link to="/notes_card" variant="primary" onClick={() => handleIndRestaurant([restaurant.alias, restaurant.order, restaurant.status])} style={{ width: '360px' }}>
+              <img
+                src={`${restaurant.img}?w=248&h=100&fit=crop&auto=format`}
+                srcSet={`${restaurant.img}?w=248&h=100&fit=crop&auto=format&dpr=2 2x`}
+                alt={restaurant.name}
+                loading="lazy"
+                className="imageRest"
+              />
+            </Link>
+            <ImageListItemBar
+              title={<span style={{ fontWeight: 'bold' }}>{restaurant.name}</span>}
+              subtitle={<span>{restaurant.address}</span>}
+              position=""
+              className="imageLabel"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>;
+      </div>
+    } else {
+      pageDisplay =
+      <div style = {{  }}>
+        <br /><br />
+        <img className="eggQuestion" src={eggEmpty} alt="error..." />
+        <h2 className="errorLabel">No restaurants to display :(</h2>
+      </div>;
+    }
+
     return (
       <div style={{
         display: 'flex',
@@ -100,8 +139,8 @@ class LikedDisliked extends React.Component {
         marginTop: '8px'
       }}>
         <br />
-        <h1 style={{display: 'inline'}}>My Restaurants</h1>
-        <div style={{marginRight: '50px'}}>
+        <h1 style={{ display: 'inline' }}>My Restaurants</h1>
+        <div style={{ marginRight: '50px' }}>
           <label>
             <span>Filter search: </span>
             <input
@@ -111,7 +150,7 @@ class LikedDisliked extends React.Component {
               onChange={this.onChangeHandler}
             />
           </label>
-          <label style={{marginLeft: '30px'}}>
+          <label style={{ marginLeft: '30px' }}>
             <input
               id="likedCheck"
               type="radio"
@@ -121,7 +160,7 @@ class LikedDisliked extends React.Component {
             />
             <span className="likeDislikeLabel">Liked</span>
           </label>
-          <label style={{marginLeft: '20px'}}>
+          <label style={{ marginLeft: '20px' }}>
             <input
               id="dislikedCheck"
               type="radio"
@@ -132,31 +171,9 @@ class LikedDisliked extends React.Component {
             <span className="likeDislikeLabel">Disliked</span>
           </label>
         </div>
-        
-        <div style={{marginLeft: '4.5%'}}>
-          <br />
-          <ImageList cols={3} sx={{ width: '1300px' }}>
-            {restaurantList?.map((restaurant) => (
-              <ImageListItem key={restaurant.img} style={{marginBottom: '40px'}}>
-                <Link to="/notes_card" variant="primary" onClick={() => handleIndRestaurant([restaurant.alias, restaurant.order, restaurant.status])} style={{width: '360px'}}>
-                  <img
-                    src={`${restaurant.img}?w=248&h=100&fit=crop&auto=format`}
-                    srcSet={`${restaurant.img}?w=248&h=100&fit=crop&auto=format&dpr=2 2x`}
-                    alt={restaurant.name}
-                    loading="lazy"
-                    className="imageRest"
-                  />
-                </Link>
-                <ImageListItemBar
-                  title={<span style={{fontWeight: 'bold'}}>{restaurant.name}</span>}
-                  subtitle={<span>{restaurant.address}</span>}
-                  position=""
-                  className="imageLabel"
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-        </div>
+
+          {pageDisplay}
+
       </div>
     )
   }
